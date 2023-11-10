5c0984ee8b78a930fb0c25cb7261559847de6036
prideravi2001 <53620282+prideravi2001@users.noreply.github.com>
2023-11-10 01:47:39 +0000

    added a signature in the code

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+/** 
+  Author :- Ravi Kant
+  LinkedIn :- www.linkedin.com/in/ravi-2001
+  Role :- FE SE
+*/
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
